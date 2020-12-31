b1eb71b97dd32bb1445c09b3f9869b42ea855560
MISTIKMAKS <mistikmaks80885@gmail.com>
2020-12-31 12:30:19 +0200

    first master commit

---
+++ pr6_3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+template <typename T>
+T it(T P, T k)
+{
+	return P + k;
+}
+int array_result(int A[], int P) {
+	for (int i = 0; i < 10; i++)
+	{
+		A[i] = rand() % 90 + 5;
+		if (A[i] % 2 == 1)
+		{
+			P = it(P, 1);
+		}
+	}
+	return P;
+}
+int output(int A[], int P) {
+	cout << "Array: ";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "[" << i << "] " << A[i] << "   ";
+	}
+	return 1;
+}
+int main() {
+	int A[10];
+	int P = 0;
+	cout << "Result: " << array_result(A, P) << endl;
+	cout << output(A, P) << endl;
+	return 0;
+}
